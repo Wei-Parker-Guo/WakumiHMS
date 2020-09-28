c6adce7b58005fe7d4f473199f5698e0e527dd25
Wei Guo <wei.parker.guo.sg@gmail.com>
2020-09-28 23:30:51 +0800

    [Repo]changed .gitignore to include source code.

---
+++ src/server/WakumiHMSserver.cpp
@@ -0,0 +1,12 @@
+﻿// WakumiHMS.cpp : Defines the entry point for the application.
+//
+
+#include "WakumiHMSserver.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
